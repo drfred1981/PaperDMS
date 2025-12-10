e0291db51d01cf1c1eb39b6976abba4a2ed67460
drfred1981 <you@example.com>
2025-12-10 06:56:11 +0000

    Initial version of reportingService generated by generator-jhipster@9.0.0-beta.0

---
+++ reportingService/src/main/java/fr/smartprod/paperdms/reporting/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package fr.smartprod.paperdms.reporting.aop.logging;
---
+++ reportingService/src/main/java/fr/smartprod/paperdms/reporting/config/Constants.java
@@ -0,0 +1,12 @@
+package fr.smartprod.paperdms.reporting.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+    public static final String DEFAULT_LANGUAGE = "en";
+
+    private Constants() {}
+}
