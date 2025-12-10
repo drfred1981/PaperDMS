7332a2350399316164cf947f45ba67ea193adab5
drfred1981 <you@example.com>
2025-12-10 06:56:07 +0000

    Initial version of emailImportService generated by generator-jhipster@9.0.0-beta.0

---
+++ emailImportService/src/main/java/fr/smartprod/paperdms/emailimport/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer.
+ */
+package fr.smartprod.paperdms.emailimport.web.rest;
---
+++ emailImportService/src/test/java/fr/smartprod/paperdms/emailimport/config/EmbeddedKafka.java
@@ -0,0 +1,10 @@
+package fr.smartprod.paperdms.emailimport.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EmbeddedKafka {}
